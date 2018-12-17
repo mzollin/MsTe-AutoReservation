95f711a55e3a1558b85fabf8d9bc916198e2a75c
Michael Stocker <mic.sto@hotmail.com>
2018-12-17 11:38:06 +0100

    Added Generic Fault & Fault-Contracts to Interface

---
+++ AutoReservation.Common/DataTransferObjects/Faults/AutoReservationFault.cs
@@ -0,0 +1,25 @@
+﻿using System.Runtime.Serialization;
+
+namespace AutoReservation.Common.DataTransferObjects.Faults
+{
+    [DataContract]
+    class AutoReservationFault
+    {
+        #region ErrorCodeConstants
+        //Data Integrity
+        public const string RequestedEntityDoesNotExist =   "ARE-D-001";
+        public const string DatabaseTimestampsNotMatching = "ARE-D-002";
+        public const string NegativeDateRange =             "ARE-D-003";
+        public const string CarNotAvailable =               "ARE-D-004";
+        public const string BirthdayInTheFuture =           "ARE-D-005";
+
+        //Faulty Arguments
+        public const string LuxuryCarMissingBaseRate =      "ARE-A-001";
+        public const string CarMustNotHaveBaseRate =        "ARE-A-002";
+        public const string CustomerNot18YearsOld =         "ARE-A-004";
+        #endregion
+
+        [DataMember]
+        public string ErrorCode { get; set; }
+    }
+}
--- AutoReservation.Common/Interfaces/IAutoReservationService.cs
+++ AutoReservation.Common/Interfaces/IAutoReservationService.cs
@@ -1,6 +1,7 @@
 ﻿using AutoReservation.Common.DataTransferObjects;
 using System.Collections.Generic;
 using System.ServiceModel;
+using AutoReservation.Common.DataTransferObjects.Faults;
 
 namespace AutoReservation.Common.Interfaces
 {
@@ -8,40 +9,53 @@ namespace AutoReservation.Common.Interfaces
     public interface IAutoReservationService
     {
         [OperationContract]
+        [FaultContract(typeof(AutoReservationFault))]
         void Create(AutoDto car);
         [OperationContract]
+        [FaultContract(typeof(AutoReservationFault))]
         void Create(ReservationDto reservation);
         [OperationContract]
+        [FaultContract(typeof(AutoReservationFault))]
         void Create(KundeDto customer);
 
         [OperationContract]
+        [FaultContract(typeof(AutoReservationFault))]
         AutoDto ReadCar(int id);
         [OperationContract]
         IEnumerable<AutoDto> ReadAllCars();
         [OperationContract]
+        [FaultContract(typeof(AutoReservationFault))]
         ReservationDto ReadReservation(int id);
         [OperationContract]
         IEnumerable<ReservationDto> ReadAllReservations();
         [OperationContract]
+        [FaultContract(typeof(AutoReservationFault))]
         KundeDto ReadCustomer(int id);
         [OperationContract]
         IEnumerable<KundeDto> ReadAllCustomers();
 
         [OperationContract]
+        [FaultContract(typeof(AutoReservationFault))]
         void Update(AutoDto car);
         [OperationContract]
+        [FaultContract(typeof(AutoReservationFault))]
         void Update(ReservationDto reservation);
         [OperationContract]
+        [FaultContract(typeof(AutoReservationFault))]
         void Update(KundeDto customer);
 
         [OperationContract]
+        [FaultContract(typeof(AutoReservationFault))]
         void Delete(AutoDto car);
         [OperationContract]
+        [FaultContract(typeof(AutoReservationFault))]
         void Delete(ReservationDto reservation);
         [OperationContract]
+        [FaultContract(typeof(AutoReservationFault))]
         void Delete(KundeDto customer);
 
         [OperationContract]
+        [FaultContract(typeof(AutoReservationFault))]
         bool IsCarAvailable(int id);
         [OperationContract]
         IEnumerable<AutoDto> GetAllAvailableCars();
